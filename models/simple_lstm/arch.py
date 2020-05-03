f56609b3d97e4badb8a1b1a595fc6bfd50fd7827
Anna Kirichenko <kianya13@gmail.com>
2020-05-03 00:45:02 +0000

    tutorial model

---
+++ models/simple_lstm/arch.py
@@ -0,0 +1,16 @@
+from fairseq.models import register_model_architecture
+
+# The first argument to ``register_model_architecture()`` should be the name
+# of the model we registered above (i.e., 'simple_lstm'). The function we
+# register here should take a single argument *args* and modify it in-place
+# to match the desired architecture.
+
+@register_model_architecture('simple_lstm', 'tutorial_simple_lstm')
+def tutorial_simple_lstm(args):
+    # We use ``getattr()`` to prioritize arguments that are explicitly given
+    # on the command-line, so that the defaults defined below are only used
+    # when no other value has been specified.
+    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 256)
+    args.encoder_hidden_dim = getattr(args, 'encoder_hidden_dim', 256)
+    args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', 256)
+    args.decoder_hidden_dim = getattr(args, 'decoder_hidden_dim', 256)
---
+++ train_simple_lstm.sh
@@ -0,0 +1,5 @@
+fairseq-train data-bin/iwslt14.tokenized.de-en \
+  --arch tutorial_simple_lstm \
+  --encoder-dropout 0.2 --decoder-dropout 0.2 \
+  --optimizer adam --lr 0.005 --lr-shrink 0.5 \
+  --max-tokens 12000
