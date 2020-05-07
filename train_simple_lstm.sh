59df8ca85a45932c07a710eac928437d52e73ddb
Anna Kirichenko <kianya13@gmail.com>
2020-05-07 00:52:37 +0000

    run tutorial fairseq

--- train_simple_lstm.sh
+++ train_simple_lstm.sh
@@ -1,4 +1,5 @@
 fairseq-train data-bin/iwslt14.tokenized.de-en \
+  --user-dir /home/kianya/text_summarization/models/simple_lstm \
   --arch tutorial_simple_lstm \
   --encoder-dropout 0.2 --decoder-dropout 0.2 \
   --optimizer adam --lr 0.005 --lr-shrink 0.5 \
